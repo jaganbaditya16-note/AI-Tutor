83c54b4001caec4d55d9abab78dfcb47c430a38c
Baditya Jagan Panchanan <jaganbaditya16@gmail.com>
2026-09-05 16:12:37 +0530

    Add shared Clerk server authorization helper

---
+++ lib/auth.ts
@@ -0,0 +1,7 @@
+import { auth } from "@clerk/nextjs/server";
+
+export async function requireUser() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("UNAUTHORIZED");
+  return userId;
+}
