2412a74d31b9e3f503268ecaf45a49990c58003c
Baditya Jagan Panchanan <jaganbaditya16@gmail.com>
2026-09-11 17:03:03 +0530

    Add usage guide link to Faculty sign-in

--- app/faculty/sign-in/page.tsx
+++ app/faculty/sign-in/page.tsx
@@ -2,6 +2,7 @@
 
 import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { createClient } from "@/lib/supabase/browser";
 
 export default function FacultySignInPage() {
